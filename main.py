b08316c0af891962f4e2d53609e0491f924a00ea
Paul Pavish <72356821+paulpavish@users.noreply.github.com>
2021-10-12 23:15:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ led=Pin(2,Pin.OUT)  #create LED object from pin2,Set Pin2 to output
 
 while True:
     led.value(1)    #turn off
-    time.sleep(0.1)
+    time.sleep(1)
     led.value(0)    #turn on
-    time.sleep(0.1)
+    time.sleep(1)
